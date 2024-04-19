34c319ce8519608f39872fa00dc3e9f937270652
IvanDrag0 <michael.perstin@gmail.com>
2024-04-19 14:08:58 -0500

    Initial Commit.

---
+++ ClassificationBanner/ABEdge.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassificationBanner {
+    public enum ABEdge : int {
+        Left = 0,
+        Top,
+        Right,
+        Bottom,
+        None
+    }
+}
---
+++ ClassificationBannerWpfExample/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using ClassificationBanner;
+
+namespace ClassificationBannerWpfExample {
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Loaded += MainWindow_Loaded;
+        }
+
+        void MainWindow_Loaded( object sender , RoutedEventArgs e )
+        {
+            //AppBar.IsEnabled = false;
+            //Normal.IsEnabled = true;
+            this.ShowInTaskbar = false;
+            AppBarFunctions.SetAppBar(this , ABEdge.Top);
+
+        }
+    }
+}
